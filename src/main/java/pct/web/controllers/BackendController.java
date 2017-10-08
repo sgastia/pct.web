9a80c6b97b339e3c0411488a441048aa4dd50577
sgastia <sgastia@gamil.com>
2017-10-08 20:15:13 -0300

    Adding initial version for backend_videos

--- src/main/java/pct/web/controllers/BackendController.java
+++ src/main/java/pct/web/controllers/BackendController.java
@@ -9,26 +9,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/backend")
 public class BackendController {
-
-	
-	/*Este anda ok
-	@RequestMapping(method = RequestMethod.GET)
-    public String index() {
-        return "backend";
-    }
-	*/
 	
 	//https://spring.io/guides/gs/handling-form-submission/
 	
 	@RequestMapping(value = {"/",""}, method = RequestMethod.GET)
-	public String backend(Model model,@RequestParam(value="name", required=false, defaultValue="<no name>") String name) {
+	public String backendIndex(Model model,@RequestParam(value="name", required=false, defaultValue="<no name>") String name) {
 		model.addAttribute("name", name);
-        return "backend";////tiene que coincidir con el nombre de la vista: pct.web\src\main\resources\templates\backend.html
+        return "backend";//pct.web\src\main\resources\templates\backend.html
 	}
 	
 	@RequestMapping(value="/conciertos",method = RequestMethod.GET)
-	public String conciertos(){
-		return "backend_conciertos";//tiene que coincidir con el nombre de la vista: pct.web\src\main\resources\templates\backend_conciertos.html
+	public String backendConciertos(){
+		return "backend_conciertos";//pct.web\src\main\resources\templates\backend_conciertos.html
+		
+	}
+	
+	@RequestMapping(value="/videos",method = RequestMethod.GET)
+	public String backendVideos(){
+		return "backend_videos";//pct.web\src\main\resources\templates\backend_conciertos.html
 		
 	}
 }
